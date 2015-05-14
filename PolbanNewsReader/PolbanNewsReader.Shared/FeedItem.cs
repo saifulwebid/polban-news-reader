f0380e6cf789048f240a3a85d4aa90ec3973a916
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-05-14 21:08:31 +0700

    Add FeedItem class

---
+++ PolbanNewsReader/PolbanNewsReader.Shared/FeedItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolbanNewsReader
+{
+    public class FeedItem
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime PubDate { get; set; }
+        public Uri Link { get; set; }
+    }
+}
