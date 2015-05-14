9e82f65ba5989fcad4d3653b517c4a54efe69959
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-05-14 21:35:41 +0700

    Add FeedDataSource class

---
+++ PolbanNewsReader/PolbanNewsReader.Shared/FeedDataSource.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+
+namespace PolbanNewsReader
+{
+    public class FeedDataSource
+    {
+        private ObservableCollection<FeedItem> _Feeds = new ObservableCollection<FeedItem>();
+        public ObservableCollection<FeedItem> Feeds
+        {
+            get { return _Feeds; }
+        }
+
+        public async Task<List<FeedItem>> GetFeedsAsync()
+        {
+            SyndicationClient client = new SyndicationClient();
+            Uri feedUri = new Uri("http://www.polban.ac.id/index.php?format=feed&type=rss");
+            List<FeedItem> list = new List<FeedItem>();
+
+            try
+            {
+                SyndicationFeed feed = await client.RetrieveFeedAsync(feedUri);
+
+                if (feed.Items != null && feed.Items.Count > 0)
+                {
+                    foreach (SyndicationItem item in feed.Items)
+                    {
+                        FeedItem feedItem = new FeedItem();
+
+                        if (item.Title != null && item.Title.Text != null) {
+                            feedItem.Title = item.Title.Text;
+                        }
+
+                        if (item.Authors != null && item.Authors.Count > 0) {
+                            feedItem.Author = item.Authors[0].Name.ToString();
+                        }
+
+                        if (item.Summary != null && item.Summary.Text != null) {
+                            feedItem.Content = item.Summary.Text;
+                        }
+
+                        if (item.PublishedDate != null) {
+                            feedItem.PubDate = item.PublishedDate.DateTime;
+                        }
+
+                        if (item.Links != null && item.Links.Count > 0) {
+                            feedItem.Link = item.Links[0].Uri;
+                        }
+
+                        list.Add(feedItem);
+                    }
+                }
+
+                return list;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static FeedItem GetItem(string title)
+        {
+            var _feedDataSource = App.Current.Resources["feedDataSource"] as FeedDataSource;
+            var matches = _feedDataSource.Feeds.Where((item) => item.Title.Equals(title));
+
+            if (matches.Count() > 1) return matches.First();
+            return null;
+        }
+    }
+}
